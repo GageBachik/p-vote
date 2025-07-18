6fb5cbca08b28ecb1a0aadb5e33fe2a7f30170f5
GageBachik <loser@gagebachik.com>
2025-07-17 17:32:30 -0700

    updated benches

--- benches/compute_units.md
+++ benches/compute_units.md
@@ -1,3 +1,23 @@
+#### 2025-07-17 23:23:07.967768 UTC
+
+Solana CLI Version: solana-cli 2.2.19 (src:8213d013; feat:3073396398, client:Agave)
+
+| Name | CUs | Delta |
+|------|------|-------|
+| InitializePlatform | 2944 | -- |
+| UpdatePlatform | 453 | -- |
+| InitializeVote | 16155 | - new - |
+
+#### 2025-07-17 23:17:16.990051 UTC
+
+Solana CLI Version: solana-cli 2.2.19 (src:8213d013; feat:3073396398, client:Agave)
+
+| Name | CUs | Delta |
+|------|------|-------|
+| InitializePlatform | 2944 | +111 |
+| UpdatePlatform | 453 | +54 |
+| UpdatePlatform | 16155 | +15,756 |
+
 #### 2025-07-16 04:01:10.649709 UTC
 
 Solana CLI Version: solana-cli 2.2.19 (src:8213d013; feat:3073396398, client:Agave)
--- benches/compute_units.rs
+++ benches/compute_units.rs
@@ -3,9 +3,11 @@ use mollusk_svm_bencher::MolluskComputeUnitBencher;
 
 use p_vote::instructions::{initialize_platform::InitializePlatformInstructionData, update_platform::UpdatePlatformInstructionData};
 use p_vote::state::{Platform, PLATFORM_SEED};
-use p_vote::ID;
+use p_vote::{InitializeVoteInstructionData, ID};
 
-use solana_sdk::account::AccountSharedData;
+use solana_sdk::account::{AccountSharedData, WritableAccount};
+use solana_sdk::program_option::COption;
+use solana_sdk::program_pack::Pack;
 use solana_sdk::{
     account::Account,
     instruction::{AccountMeta, Instruction},
@@ -29,7 +31,12 @@ pub fn get_rent_data() -> Vec<u8> {
 }
 
 fn main() {
-    let mollusk = Mollusk::new(&PROGRAM, "target/deploy/p_vote");
+    let mut mollusk = Mollusk::new(&PROGRAM, "target/deploy/p_vote");
+    mollusk.add_program(
+        &spl_token::ID,
+        "tests/elf_files/spl_token",
+        &mollusk_svm::program::loader_keys::LOADER_V3,
+    );
 
      //system program and system account
      let (system_program, system_account) = program::keyed_account_for_system_program();
@@ -91,9 +98,8 @@ fn main() {
         (system_program, system_account.clone()),
     ];
 
-    // ########################## Next IX
- 
- 
+    //  ##################################### Next Ix
+
      //Initialize the accounts
      let old_authority_account = Account::new(1 * LAMPORTS_PER_SOL, 0, &system_program);
      let new_authority_account = Account::new(2 * LAMPORTS_PER_SOL, 0, &system_program);
@@ -151,9 +157,134 @@ fn main() {
 
     //  ##################################### Next Ix
 
+    //system program and system account
+    let (system_program, system_account) = program::keyed_account_for_system_program();
+
+    // token program and token account
+    let (token_program, token_account) = (
+        spl_token::ID,
+        program::create_program_account_loader_v3(&spl_token::ID),
+    );
+
+    // mint-fake usdc
+    let mint_usdc = Pubkey::new_from_array([0x02; 32]);
+    let mut mint_usdc_account = Account::new(
+        mollusk
+            .sysvars
+            .rent
+            .minimum_balance(spl_token::state::Mint::LEN),
+        spl_token::state::Mint::LEN,
+        &token_program,
+    );
+    solana_sdk::program_pack::Pack::pack(
+        spl_token::state::Mint {
+            mint_authority: COption::None,
+            supply: 100_000_000,
+            decimals: 6,
+            is_initialized: true,
+            freeze_authority: COption::None,
+        },
+        mint_usdc_account.data_as_mut_slice(),
+    )
+    .unwrap();
+
+
+    // Create the Platform PDA
+    let (platform_pda, platform_bump) =
+        Pubkey::find_program_address(&[PLATFORM_SEED], &PROGRAM);
+    println!("platform: {:?}", platform_pda);
+
+    // Create the vault PDA
+    let (vault_pda, vault_bump) =
+    Pubkey::find_program_address(&[platform_pda.to_bytes().as_ref()], &PROGRAM);
+    println!("vault: {:?}", vault_pda);
+
+
+    //Initialize the accounts
+    let authority_account = Account::new(1 * LAMPORTS_PER_SOL, 0, &system_program);
+    let platform_init_state = Platform {
+        authority: AUTHORITY.to_bytes(),
+        fee: (100 as u16).to_le_bytes(),
+        platform_bump: platform_bump,
+        vault_bump: vault_bump
+    };
+    let mut platform_account = AccountSharedData::new(
+        mollusk.sysvars.rent.minimum_balance(Platform::LEN),
+        Platform::LEN,
+        &PROGRAM
+    );
+    platform_account.set_data_from_slice(bytemuck::bytes_of(&platform_init_state));
+
+
+    let vote_key = Pubkey::new_unique();
+    let vote_account = Account::new(0, 0, &system_program);
+    // Create the vote vault PDA
+    let (vote_vault_pda, _vote_vault_bump) =
+    Pubkey::find_program_address(&[vote_key.to_bytes().as_ref()], &PROGRAM);
+    let vote_vault_account = Account::new(0, 0, &system_program);
+
+    let vault_account = Account::new(( 0.01 * 1e9 ) as u64, 0, &system_program);
+    let min_balance = mollusk.sysvars.rent.minimum_balance(Rent::size_of());
+    let mut rent_account = Account::new(min_balance, Rent::size_of(), &RENT);
+    rent_account.data = get_rent_data();
+
+    // Create the vault PDA
+    let (vote_vault_token_pda, _vote_vault_token_bump) =
+    Pubkey::find_program_address(&[
+        vote_vault_pda.as_ref(),
+        spl_token::ID.as_ref(),
+        mint_usdc.as_ref(),
+    ], &spl_associated_token_account::ID);
+    let vote_vault_token_account = Account::new(0, 0, &system_program);
+
+    //Push the accounts in to the instruction_accounts vec!
+    let initialize_vote_ix_accounts = vec![
+        AccountMeta::new(AUTHORITY, true),
+        AccountMeta::new(platform_pda, false),
+        AccountMeta::new(vault_pda, false),
+        AccountMeta::new(vote_key, true),
+        AccountMeta::new_readonly(mint_usdc, false),
+        AccountMeta::new(vote_vault_pda, true),
+        AccountMeta::new(vote_vault_token_pda, true),
+        // Required for ix
+        AccountMeta::new_readonly(RENT, false),
+        AccountMeta::new_readonly(system_program, false),
+        AccountMeta::new_readonly(token_program, false),
+    ];
+
+    // Create tx_accounts vec
+    let initialize_vote_tx_accounts = &vec![
+        (AUTHORITY, authority_account.clone()),
+        (platform_pda, platform_account.clone().into()),
+        (vault_pda, vault_account.clone()),
+        (vote_key, vote_account.clone()),
+        (mint_usdc, mint_usdc_account.clone()),
+        (vote_vault_pda, vote_vault_account.clone()),
+        (vote_vault_token_pda, vote_vault_token_account.clone()),
+        (RENT, rent_account.clone()),
+        (system_program, system_account.clone()),
+        (token_program, token_account.clone()),
+    ];
+
+    // Create the instruction data
+    let initialize_vote_instruction_data = InitializeVoteInstructionData {
+        time_to_add: 3600i64.to_le_bytes()
+    };
+    // Bytemuck serialize the data
+    let initialize_vote_instruction_data_bytes = bytemuck::bytes_of(&initialize_vote_instruction_data);
+    // add the discriminator to tell it what ix to run 2 => initialize_vote
+    let initialize_vote_instruction_data = [vec![2], initialize_vote_instruction_data_bytes.to_vec()].concat();
+    // Create instruction
+    let initialize_vote_instruction = Instruction::new_with_bytes(PROGRAM, &initialize_vote_instruction_data, initialize_vote_ix_accounts);
+
+
+    //  ##################################### Next Ix
+
+
     MolluskComputeUnitBencher::new(mollusk)
         .bench(("InitializePlatform", &initialize_platform_instruction, initialize_platform_tx_accounts))
         .bench(("UpdatePlatform", &update_platform_instruction, update_platform_tx_accounts))
+        .bench(("InitializeVote", &initialize_vote_instruction, initialize_vote_tx_accounts))
         .must_pass(true)
         .out_dir("benches/")
         .execute();
