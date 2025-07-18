7f4ea9413c8ea141e2285a316a242a2d39ffcffd
GageBachik <loser@gagebachik.com>
2025-07-18 16:43:15 -0700

    disable import/export for vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,16 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
+ 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      'import/export': 'off'
+    },
+  }),
+]
+ 
+export default eslintConfig
